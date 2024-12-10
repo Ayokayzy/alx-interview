6d5aac129c5b343e1224be45939e0637f1b47d5c
Ayokayzy <theayokayzy1@gmail.com>
2024-12-10 15:50:12 +0100

    Task 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -16,7 +16,7 @@ def makeChange(coins: [int], total: int) -> int:
     coins.sort(reverse=True)
     change = 0
     for coin in coins:
-        if total  <= 0:
+        if total <= 0:
             break
         temp = total // coin
         change += temp
